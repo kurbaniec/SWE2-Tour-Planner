1c5b5d267f311c9b54a52a25105f8672a3207320
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2021-03-13 18:50:14 +0100

    Split main view into multiple components MainWindow now imports custom UserControls for the menu bar and main content.

---
+++ Client/View/AppContent.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Client.View
+{
+    public partial class AppContent : UserControl
+    {
+        public AppContent()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Client/View/AppMenu.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Client.View
+{
+    public partial class AppMenu : UserControl
+    {
+        public AppMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
