398ca59902d9aff355d0f8aac7c13a3a492c8b0e
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2021-03-31 17:41:19 +0200

    Working on improving Tour log information layout

---
+++ Client/Utils/Converters/ItemSourceConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+
+namespace Client.Utils.Converters
+{
+    /// <summary>
+    /// Returns the single given item as a List so it can be used inside a DataGrid.
+    /// Inspired by https://stackoverflow.com/a/34752584/12347616.
+    /// </summary>
+    public class ItemSourceConverter : MarkupExtension, IValueConverter
+    {
+        private static ItemSourceConverter? instance;
+        
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new List<object>(){ value };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return instance ??= new ItemSourceConverter();
+        }
+    }
+}
--- Client/ViewModels/ListViewModel.cs
+++ Client/ViewModels/ListViewModel.cs
@@ -92,12 +92,23 @@ namespace Client.ViewModels
                             TimeSpan.Zero, 
                             100,
                             10,
+                            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+                            55.0,
+                            60,
+                            100,
+                            2
+                        ), new TourLog(
+                            DateTime.Now, 
+                            Type.Car,
+                            TimeSpan.Zero, 
+                            100,
+                            10,
                             "Great",
                             55.0,
                             60,
                             100,
                             2
-                )}
+                    )}
             );
             var tour2 = new TourViewModel(
                 "TourB", 
