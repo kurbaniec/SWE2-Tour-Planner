e6af597f65dd0c5b368a054ccb5df32eb40874ff
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2021-04-10 17:24:56 +0200

    Moved `Client` to new directory and added test project for it

---
+++ Client/Client-Test/Unit/TourWrapperTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Client_Test.Unit
+{
+    public class TourWrapperTest
+    {
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            
+        }
+        
+        [Test, TestCase(TestName = "Dummy", Description =
+             "Dummy"
+         )]
+        public void BattlePlayerAWin()
+        {
+            Assert.IsTrue(true);
+        }
+        
+        
+    }
+}
