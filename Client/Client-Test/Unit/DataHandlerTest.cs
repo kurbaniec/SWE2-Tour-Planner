2ff773d5a1aeafb6e366a57c32add9aeea640e40
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2021-05-16 14:48:44 +0200

    Added unit tests for DataHandler

--- Client/Client-Test/Unit/DataHandlerTest.cs
+++ Client/Client-Test/Unit/DataHandlerTest.cs
@@ -1,4 +1,11 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Client.Logic.BL;
+using Client.Logic.DAL;
+using Model;
+using Moq;
 using NUnit.Framework;
 
 namespace Client_Test.Unit
@@ -6,21 +13,13 @@ namespace Client_Test.Unit
     public class DataHandlerTest
     {
         private string basePath;
-        private string routePath;
-        private string exportPath;
-        private string connString;
-        
+
         [OneTimeSetUp]
         public void Setup()
         {
             basePath = $"{Path.GetTempPath()}tp";
-            routePath = $"{basePath}{Path.DirectorySeparatorChar}maps";
-            if (!Directory.Exists(routePath))
-                Directory.CreateDirectory(routePath);    
-            exportPath = $"{basePath}{Path.DirectorySeparatorChar}exports";
-            if (!Directory.Exists(exportPath))
-                Directory.CreateDirectory(exportPath);
-            connString = string.Empty;
+            if (!Directory.Exists(basePath))
+                Directory.CreateDirectory(basePath); 
         }
 
         [OneTimeTearDown]
@@ -30,23 +29,58 @@ namespace Client_Test.Unit
                 Directory.Delete(basePath, true);
         }
         
-        [Test, TestCase(TestName = "Export Tour Report", Description =
-             "Export Tour Report (without Route Image)."
+        [Test, TestCase(TestName = "Export Tour Data", Description =
+             "Serialize & Export Tour Data to filesystem."
          )]
-        public void ExportFullReport()
+        public async Task ExportTours()
         {
-            /**var cfg = new Configuration(routePath, exportPath, connString);
-            var db = new MockDataManagement();
-            var map = new MockMapApi();
-            var handler = new PdfExportHandler(cfg);
-            var tp = new TourPlannerServer(db, map, handler);
+            var api = new Mock<ITourApi>().Object;
+            var filter = new Mock<IFilter>().Object;
+            var handler = new DataHandler();
+            var tp = new TourPlannerClient(api, handler, filter);
+            var tours = new List<Tour>()
+            {
+                new(1000, "From", "To", "Name", 10.0, "",
+                    new List<TourLog>())
+            };
+            var path = $"{basePath}{Path.DirectorySeparatorChar}export.td";
 
-            var (path, error) = tp.GetPdfExport(1000);
-
+            var (ok, error) = await tp.ExportTours(path, tours);
+            
             Assert.AreEqual("", error);
-            Assert.AreNotEqual("", path);
-            Assert.IsTrue(File.Exists(path));*/
-            Assert.IsTrue(true);
+            Assert.IsTrue(ok);
+        }
+        
+        [Test, TestCase(TestName = "Export & Import Tour Data", Description =
+             "Serialize, Export Tour Data to filesystem and Deserialize, Import it later on."
+         )]
+        public async Task ExportAndImportTours()
+        {
+            var api = new Mock<ITourApi>().Object;
+            var filter = new Mock<IFilter>().Object;
+            var handler = new DataHandler();
+            var tp = new TourPlannerClient(api, handler, filter);
+            var tours = new List<Tour>()
+            {
+                new(1000, "From", "To", "Name", 10.0, "",
+                    new List<TourLog>())
+            };
+            var path = $"{basePath}{Path.DirectorySeparatorChar}export.td";
+
+            var (ok, exportError) = await tp.ExportTours(path, tours);
+            var (importedTours, importError) = await tp.ImportTours(path);
+            
+            Assert.AreEqual("", exportError);
+            Assert.IsTrue(ok);
+            Assert.AreEqual("", importError);
+            Assert.IsNotNull(importedTours);
+            // Do not check for equality because the id of imported Tours is always 0
+            Assert.AreEqual(tours.First().From, importedTours!.First().From);
+            Assert.AreEqual(tours.First().To, importedTours!.First().To);
+            Assert.AreEqual(tours.First().Name, importedTours!.First().Name);
+            Assert.AreEqual(tours.First().Distance, importedTours!.First().Distance);
+            Assert.AreEqual(tours.First().Description, importedTours!.First().Description);
+            Assert.AreEqual(tours.First().Logs, importedTours!.First().Logs);
         }
     }
 }
